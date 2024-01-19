5fd920f9ce3333fda516d51b65aacfe07c3b20b6
favalentinuzzi <fabio.valentinuzzi@unibz.it>
2024-01-19 13:51:30 +0100

    upload

---
+++ qr_code.R
@@ -0,0 +1,64 @@
+library(qrcode)
+
+
+qr_code_E011 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E011.html")
+
+generate_svg(qrcode = qr_code_E011, filename = "E011.svg", size = 500, show = FALSE)
+
+
+qr_code_E012b <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E012b.html")
+
+generate_svg(qrcode = qr_code_E012b, filename = "E012b.svg", size = 500, show = FALSE)
+
+
+qr_code_E020 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E020.html")
+
+generate_svg(qrcode = qr_code_E020, filename = "E020.svg", size = 500, show = FALSE)
+
+
+qr_code_E021 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E021.html")
+
+generate_svg(qrcode = qr_code_E021, filename = "E021.svg", size = 500, show = FALSE)
+
+
+qr_code_E022 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E022.html")
+
+generate_svg(qrcode = qr_code_E022, filename = "E022.svg", size = 500, show = FALSE)
+
+
+qr_code_E112 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E112.html")
+
+generate_svg(qrcode = qr_code_E112, filename = "E112.svg", size = 500, show = FALSE)
+
+
+qr_code_E120 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E120.html")
+
+generate_svg(qrcode = qr_code_E120, filename = "E120.svg", size = 500, show = FALSE)
+
+
+qr_code_E122 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E122.html")
+
+generate_svg(qrcode = qr_code_E122, filename = "E122.svg", size = 500, show = FALSE)
+
+
+qr_code_E123 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E123.html")
+
+generate_svg(qrcode = qr_code_E123, filename = "E123.svg", size = 500, show = FALSE)
+
+
+qr_code_E231 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_E231.html")
+
+generate_svg(qrcode = qr_code_E231, filename = "E231.svg", size = 500, show = FALSE)
+
+
+qr_code_A2303c <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_A2303c.html")
+
+generate_svg(qrcode = qr_code_A2303c, filename = "A2303c.svg", size = 500, show = FALSE)
+
+
+qr_code_A2409 <- qr_code("https://stramon1um.github.io/FaST_Lab/BZ_A2409.html")
+
+generate_svg(qrcode = qr_code_A2409, filename = "A2409.svg", size = 500, show = FALSE)
+
+
+
